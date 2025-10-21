19b3f3e18e9a207d00f864f234c0a4c540e877b0
Caua theodoro <caua82477@gmail.com>
2025-10-21 16:15:00 -0300

    adicionando listar no streamlit

--- front-end/app.py
+++ front-end/app.py
@@ -1,6 +1,20 @@
+import streamlit as st
+import requests
+
 API_URL = "http://127.0.0.1:8000"
 st.set_page_config(page_title="Gerenciador de produtos", page_icon="📦🧾")
 st.title("📦 Estoque de produtos ")
 
 menu = st.sidebar.radio("Navegação", ["Estoque", "Registrar produto", "Atualizar produto", "Deletar produto"])
 
+if menu == "Estoque":
+    st.subheader("Todos os filmes disponiveis")
+    response = requests.get(f"{API_URL}/listar_produtos")
+    if response.status_code == 200:
+        produtos = response.json().get("produtos", [])
+        if produtos:
+            st.dataframe(produtos)
+        else:
+            st.info("❌ Não há produtos registrados")
+    else:
+        st.error("❌ Erro ao acessar a API")
