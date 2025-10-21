329156f6776505bcfe1c785b06f8a24d123113a4
Caua theodoro <caua82477@gmail.com>
2025-10-21 14:42:24 -0300

    inserindo função na conexão py

--- front-end/app.py
+++ front-end/app.py
@@ -1,6 +1,6 @@
-if menu == "Adicionar filmes":
-    st.subheader("🗓Adicionar filmes")
-    titulo = st.text_input ("Titulo do Filme")
+if menu == "Adicionar prduto":
+    st.subheader("🗓Adicionar produto")
+    titulo = st.text_input ("Titulo do produto")
     genero = st.text-input("Genero")
     ano = st.number_input("Ano de Lançamento", min_value=1880, max_value=2100, step=0.1)
     avaliacao = st.number_input("Avaliação de (0 a 10)", min_value=0.0, max_value1=0.0, step=0.1)
@@ -11,8 +11,3 @@ if menu == "Adicionar filmes":
             st.sucess("Filme adicionando com sucesso!")
         else:
             st.error("Erro ao adicionar o filme")
-
-
-@app.put("/filmes/{id_filmes}")
-def atualizar_filme(id_filme, nova_avaliacao):
-    funcao.atualizar
